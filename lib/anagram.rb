900bc4c806aaf5b66ef2bd252392db858890c719
Chad Lengefeld <chadlengefeld@gmail.com>
2019-11-20 07:22:01 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+require "pry"
+class Anagram
+  attr_accessor :words
+
+  def initialize(words)
+    @words = words
+  end
+
+  def match
+  #  binding.pry
+  @words.sort.each do |word|
+    word.split("")
+    if word == @words
+      puts word
+
+  #  m = (/1/).match(word)
+  #  if m == @word
+  #    puts m
+   end
+    end
+  end
+end
